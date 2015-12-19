60ceb614d152c1c9c9e01bf1a486ccd5d18c8e8c
Nathan Martindale (WildfireXIII) <nmblenderdude0@gmail.com>
2015-12-19 14:50:29 -0500

    Finished base setup.bat (no testing done yet)

--- src/setup.bat
+++ src/setup.bat
@@ -1,17 +1,17 @@
 @echo off
-rem -------------------------------------------
-rem		Labs-Get setup script v0.1.0-w
-rem		Date Created: 12/19/2015
-rem		Date Edited: 12/19/2015
-rem		Copyright © 2015 Digital Warrior Labs
-rem		Author: Nathan Martindale (WildfireXIII)
-rem		Description: Setup script for the windows labs-get package manager.
-rem			Run this on first-time use, or if files/env variables are missing
-rem -------------------------------------------
+::-------------------------------------------
+::		Labs-Get setup script v0.1.0-w
+::		Date Created: 12/19/2015
+::		Date Edited: 12/19/2015
+::		Copyright © 2015 Digital Warrior Labs
+::		Author: Nathan Martindale (WildfireXIII)
+::		Description: Setup script for the windows labs-get package manager.
+::			Run this on first-time use, or if files/env variables are missing
+::-------------------------------------------
 
-rem PREREQ: MUST HAVE GIT INSTALLED in order to run this setup
+:: PREREQ: MUST HAVE GIT INSTALLED in order to run this setup
 
-rem check existence of environment variables
+:: check existence of environment variables
 echo Checking environment variables...
 :ENVCHECK
 	if "%BIN_DIR%" == "" goto CREATEBIN
@@ -22,23 +22,25 @@ echo Checking environment variables...
 	echo Environment variables ready
 	goto PATHCHECK
 
-rem make sure tht the BIN_DIR exists within the path variable
+:: make sure tht the BIN_DIR exists within the path variable
 :PATHCHECK
 	echo Checking PATH...
-	rem insert clean runnable to bin folder and see if possible to run
-	echo echo Bin directory is in path > %BIN_DIR%\path_check.bat
-	path_check.bat 2> NUL rem hides error if not found
+	:: insert clean runnable to bin folder and see if possible to run
+	echo echo Bin directory is in path! > %BIN_DIR%\path_check.bat
+	path_check.bat 2> NUL :: hides error if not found
 	if errorlevel 1 goto ADDTOPATH
-
+	del %BIN_DIR%\path_check.bat
 	echo Copying primary program files...
 	goto COPYPRGM
 	
+:: move the powershell script into bin folder
 :COPYPRGM
 	copy labs-get.ps1 %BIN_DIR%
 	echo Program files located appropriately
 	echo Setting up data files...
 	goto SETDATAFILES
 
+:: create the data directory if not found, and create all necessary data files
 :SETDATAFILES
 	if not exist %DATA_DIR%\labs-get (md %DATA_DIR%\labs-get)
 	if not exist %PKG_DIR%\labs-get-list goto GETGITLIST
@@ -47,18 +49,19 @@ rem make sure tht the BIN_DIR exists within the path variable
 	echo All data files properly installed
 	goto END
 
+:: set the powershell execution policy to remotesigned
 :SETEXECUTIONPOLICY
-	echo Labs-Get is a powershell script that requires RemoteSigned execution policy in order to run
+	echo Labs-Get is a powershell script that requires remoteSigned execution policy in order to run
 	set /p allow=Allow this script to change your powershell execution policy? (y/[n]):
 	if /I "%allow%" neq "y" goto END
 	>nul powershell.exe -executionpolicy unrestricted -command set-executionpolicy remotesigned
 	goto END
 
-
-rem ------functions------
-
+:: ------functions------
 :ADDTOPATH
 	echo Bin directory wasn't found in the PATH variable
+	setx PATH "%PATH%;%BIN_DIR%"
+	echo Added %BIN_DIR% to envrionment PATH
 	goto PATHCHECK
 
 :GETTAGS
