ead3562f98516d68e9258812b1db1080f1ec9415
Nathan Martindale (WildfireXIII) <nmblenderdude0@gmail.com>
2015-12-24 10:10:08 -0500

    Finished all needed functions for linux setup script

--- src/setup.sh
+++ src/setup.sh
@@ -2,8 +2,8 @@
 #----------------------------------------------
 #	labs-get setup script v1.0.0-lx
 #	Date Created: 12/22/2015
-#	Date Edited: 12/23/2015
-#	Copyright Â©2015 Digital Warrior Labs
+#	Date Edited: 12/24/2015
+#	Copyright Â ©2015 Digital Warrior Labs
 #	Author: Nathan Martindale (WildfireXIII)
 #	Description: Setup script for the linux labs-get package manager
 #		Run this on first-time use, or if files/env variables are missing
@@ -54,9 +54,25 @@ function setPkgDir()
 
 function addToPath()
 {
-	# TODO TODO TODO
+	PATH=$PATH:$BIN_DIR
+	echo "PATH=$PATH:$BIN_DIR" >> ~/.bashrc
 }
 
+function getGitList()
+{
+	echo "Retrieving list git repo..."
+	echo -n "Enter git url for package list: "
+	read url
+	git clone $url $PKG_DIR/labs-get-list
+	echo "List data git repo installed"
+}
+
+function getTags()
+{
+	echo -n "Enter default package tags to filter (can be changed later in the default-tags.dat file in the data directory): "
+	read tags
+	echo "$tags" > $DATA_DIR/labs-get/default-tags.dat
+}
 
 # ------------------- MAIN PROGRAM FLOW -------------------
 
@@ -72,9 +88,22 @@ source /etc/environment # update environment variables
 # make sure that the BIN_DIR exists within the path variable
 echo "Checking PATH..."
 
-#insert clean runnable to bin folder and see if possible to run
+# insert clean runnable to bin folder and see if possible to run
 echo "echo 'Bin directory is in path!'" > $BIN_DIR/path_check.sh
 chmod +x $BIN_DIR/path_check.sh
 if ! [[ path_check ]]; then
-	
+	addToPath()
 fi
+del $BIN_DIR/path_check.sh
+
+# move primary script into the bin folder so it can be run
+echo "Copying primary program files..."
+cp labs-get.sh $BIN_DIR
+echo "Program files located appropriately"
+
+# create the data directory if not found, and create all necessary data files
+echo "Setting up data files..."
+if [ ! -e "$DATA_DIR/labs-get" ]; then md $DATA_DIR/labs-get; fi
+if [ ! -e "$PKG_DIR/labs-get-list" ]; then getGitList(); fi
+if [ ! -e "$DATA_DIR/labs-get/installed.dat" ]; then echo "name,packagename,url,tags,dependencies" > $DATA_DIR/labs-get/installed.dat; fi
+if [ ! -e "$DATA_DIR/labs-get/default-tags.dat" ]; then getTags(); fi
