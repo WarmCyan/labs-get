c868758a564a84b798017281fcad45b1709cdea1
WildfireXIII <nmblenderdude0@gmail.com>
2017-05-05 09:25:11 -0500

    Changed how environment variables work with linux setup

--- src/setup.sh
+++ src/setup.sh
@@ -2,8 +2,8 @@
 #----------------------------------------------
 #	labs-get setup script v1.0.0-lx
 #	Date Created: 12/22/2015
-#	Date Edited: 12/26/2015
-#	Copyright Â ©2015 Digital Warrior Labs
+#	Date Edited: 5/5/2017
+#	Copyright © 2017 Digital Warrior Labs
 #	Author: Nathan Martindale (WildfireXIII)
 #	Description: Setup script for the linux labs-get package manager
 #		Run this on first-time use, or if files/env variables are missing
@@ -12,12 +12,44 @@
 # PREREQ: MUST HAVE GIT INSTALLED in order to run this setup
 
 # ------------------- FUNCTIONS ---------------------
+
+# NOTE: currently, this WIPES the master environment
+function createEnvironment()
+{
+	if [ ! -f ~/.env ]; then
+		echo "No environment file found"
+		touch ~/.env
+		echo "Environment file created!"
+	else
+		echo "Clearing existing environment file"
+		echo "" > ~/.env
+	fi
+}
+
+# NOTE: this should always be run at the END of the environment script, in case
+# the local has to override something
+function addLocalEnvironment()
+{
+	#echo '
+	#if [ -f $DIR_CONF/.env_l ]; then
+		#source $DIR_CONF/.env_l
+	#fi
+	#' >> ~/.env
+	echo '
+	# load local environment if it exists
+	[[ -f $DIR_CONF/.env_l ]] && . $DIR_CONF/.env_l
+	'
+}
+
 function setBinDir()
 {
 	echo "No bin path defined!"
 	echo -n "Enter path to bin folder: "
 	read binPath
-	echo "BIN_DIR=\"$binPath\"" >> /etc/environment
+	#echo "BIN_DIR=\"$binPath\"" >> /etc/environment
+	#echo "BIN_DIR=\"$binPath\"" >> ~/.bash_profile
+	
+	echo "export BIN_DIR=\"$binPath\"" >> ~/.env
 }
 
 function setLibDir()
@@ -25,7 +57,8 @@ function setLibDir()
 	echo "No lib path defined!"
 	echo -n "Enter path to lib folder: "
 	read libPath
-	echo "LIB_DIR=\"$libPath\"" >> /etc/environment
+	#echo "LIB_DIR=\"$libPath\"" >> /etc/environment
+	echo "export LIB_DIR=\"$libPath\"" >> ~/.env
 }
 
 function setConfDir()
@@ -33,7 +66,8 @@ function setConfDir()
 	echo "No conf path defined!"
 	echo -n "Enter path to conf folder: "
 	read confPath
-	echo "CONF_DIR=\"$confPath\"" >> /etc/environment
+	#echo "CONF_DIR=\"$confPath\"" >> /etc/environment
+	echo "export CONF_DIR=\"$confPath\"" >> ~/.env
 }
 
 function setDataDir()
@@ -41,7 +75,8 @@ function setDataDir()
 	echo "No data path defined!"
 	echo -n "Enter path to data folder: "
 	read dataPath
-	echo "DATA_DIR=\"$dataPath\"" >> /etc/environment
+	#echo "DATA_DIR=\"$dataPath\"" >> /etc/environment
+	echo "export DATA_DIR=\"$dataPath\"" >> ~/.env
 }
 
 function setPkgDir()
@@ -49,14 +84,16 @@ function setPkgDir()
 	echo "No pkg path defined!"
 	echo -n "Enter path to pkg folder: "
 	read pkgPath
-	echo "PKG_DIR=\"$pkgPath\"" >> /etc/environment
+	#echo "PKG_DIR=\"$pkgPath\"" >> /etc/environment
+	echo "export PKG_DIR=\"$pkgPath\"" >> ~/.env
 }
 
 function addToPath()
 {
 	echo "Bin folder not found in path, adding now..."
 	PATH=$PATH:$BIN_DIR
-	echo 'PATH=$PATH:$BIN_DIR' >> ~/.bashrc
+	#echo 'PATH=$PATH:$BIN_DIR' >> ~/.bashrc
+	echo 'export PATH=${PATH}:$BIN_DIR' >> ~/.env
 }
 
 function getGitList()
@@ -79,6 +116,7 @@ function getTags()
 
 # check existence of environment variables
 echo "Checking environment variables..."
+createEnvironment
 if [ -z ${BIN_DIR+x} ]; then 
 	setBinDir
 fi
@@ -94,7 +132,9 @@ fi
 if [ -z ${PKG_DIR+x} ]; then 
 	setPkgDir
 fi
-source /etc/environment # update environment variables
+#source /etc/environment # update environment variables
+addLocalEnvironment
+source ~/.env
 
 # make sure that the BIN_DIR exists within the path variable
 echo "Checking PATH..."
